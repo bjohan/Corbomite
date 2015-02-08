39bdb64818701569ff21e5c11f238b385820100d
bjohan <sa6bxq@gmail.com>
2015-02-08 17:17:50 +0100

    removed old code

--- trunk/src/py/hmi/corbomiteGuiWidgetFactory.py
+++ trunk/src/py/hmi/corbomiteGuiWidgetFactory.py
@@ -535,18 +535,12 @@ class CorbomiteGuiWidgetTraceIn(CorbomiteGuiWidget):
 
     def autoScale(self):
         if self.widget.name in self.traceMemory:
-            # self.xMin = min(self.traceMemory[self.widget.name][0])
-            # self.xMax = max(self.traceMemory[self.widget.name][0])
-            # self.yMin = min(self.traceMemory[self.widget.name][1])
-            # self.yMax = max(self.traceMemory[self.widget.name][1])
             self.scale.setLimits(min(self.traceMemory[self.widget.name][0]),
                                  max(self.traceMemory[self.widget.name][0]),
                                  min(self.traceMemory[self.widget.name][1]),
                                  max(self.traceMemory[self.widget.name][1]))
         else:
             self.scale.setLimits(0.0, 1.0, 0.0, 1.0)
-            # self.xMin = self.yMin = 0
-            # self.xMax = self.yMax = 1
 
     def onLeftDown(self, evt):
         self.leftPressed = True
@@ -597,14 +591,6 @@ class CorbomiteGuiWidgetTraceIn(CorbomiteGuiWidget):
                 dxp = evt.GetX()-self.lastCoords[0]
                 dyp = evt.GetY()-self.lastCoords[1]
                 self.scale.drag(self, dxp, dyp)
-                # uppx = -(self.xMax-self.xMin)/float(self.GetSize()[0])
-                # uppy = (self.yMax-self.yMin)/float(self.GetSize()[1])
-                # dx = dxp*uppx
-                # dy = dyp*uppy
-                # self.xMax += dx
-                # self.yMax += dy
-                # self.xMin += dx
-                # self.yMin += dy
         self.rePaint()
         self.lastCoords = (evt.GetX(), evt.GetY())
 
@@ -626,20 +612,6 @@ class CorbomiteGuiWidgetTraceIn(CorbomiteGuiWidget):
             self.popupPoint = pos
             self.PopupMenu(self.popupmenu, pos)
         self.scrolled = False
-
-    # def zoomYScale(self, factor):
-    #    center = 0.5*(self.yMin+self.yMax)
-    #    half = self.yMax-center
-    #    half = half/factor
-    #    self.yMin = center-half
-    #    self.yMax = center+half
-
-    # def zoomXScale(self, factor):
-    #    center = 0.5*(self.xMin+self.xMax)
-    #    half = self.xMax-center
-    #    half = half/factor
-    #    self.xMin = center-half
-    #    self.xMax = center+half
 
     def onMouseWheel(self, evt):
         self.scrolled = True
@@ -665,82 +637,6 @@ class CorbomiteGuiWidgetTraceIn(CorbomiteGuiWidget):
         dc = wx.PaintDC(self)
         self.render(wx.BufferedDC(dc))
 
-    #def findClosest125(self, value):
-    #    tens = -20
-    #    while True:
-    #        for m in [1, 2, 5]:
-    #            f = (10**tens)*m
-    #            if f > value:
-    #                return f
-    #        tens += 1
-
-    #def getGraticuleResolution(self, axisRange, axisSize):
-    #    maxGraticuleLines = axisSize/self.pixelsPerGraticuleLine
-    #    firstGuess = axisRange/maxGraticuleLines
-    #    return self.findClosest125(firstGuess)
-
-    def xAxisToPixels(self, x):
-        (winx, winy) = self.GetSize()
-        pixelsPerUnit = winx/(self.xMax-self.xMin)
-        xPixels = (x - self.xMin)*pixelsPerUnit
-        return max(min(xPixels, winx+1), -1)
-
-    def yAxisToPixels(self, y):
-        (winx, winy) = self.GetSize()
-        pixelsPerUnit = winy/(self.yMax - self.yMin)
-        yPixels = winy-(y-self.yMin)*pixelsPerUnit
-        return max(min(yPixels, winy+1), -1)
-
-    #def computeScale(self, axisMax, axisMin, axisLengthInPixels):
-    #    axisMax = float(axisMax)
-    #    axisMin = float(axisMin)
-    #    axisRange = axisMax - axisMin
-    #    if axisRange < 1e-18:
-    #            axisRange = 1
-    #    graticuleResolution = self.getGraticuleResolution(axisRange,
-    #                                                      axisLengthInPixels)
-    #    startGraticule = axisMin - axisMin % graticuleResolution
-    #    currentGraticule = startGraticule
-    #    gls = []  # Graticule lines as pixel coordinates
-    #    while True:
-    #        gls.append(currentGraticule)
-    #        currentGraticule += graticuleResolution
-    #        if currentGraticule > axisMax:
-    #            break
-    #    return (gls, graticuleResolution)
-
-    # def drawScale(self, dc):
-    #    (winx, winy) = self.GetSize()
-    #    (glsY, ry) = self.computeScale(self.yMax, self.yMin, winy)
-    #    (glsX, rx) = self.computeScale(self.xMax, self.xMin, winx)
-    #    dc.SetPen(wx.Pen("GREY", 3))
-    #    first = True
-    #    for ya in glsY[1:]:
-    #        y = self.yAxisToPixels(ya)
-    #        dc.DrawLine(0, y, winx, y)
-    #        if first:
-    #            self.y0 = y
-    #            yString = self.widget.value[1].getPrecisionString(ya, 2) + "\n"\
-    #                + self.widget.value[1].getPrecisionString(ry, 2) + "/div"
-    #            dc.DrawText(yString,
-    #                        0, y-36)
-    #            dc.SetPen(wx.Pen("GREY", 1))
-    #            first = False
-    #
-    #    dc.SetPen(wx.Pen("GREY", 3))
-    #    first = True
-    #    for xa in glsX[1:]:
-    #        x = self.xAxisToPixels(xa)
-    #        dc.DrawLine(x, 0, x, winy)
-    #        if first:
-    #            self.x0 = x
-    #            xString = self.widget.value[0].getPrecisionString(xa, 2) + "\n"\
-    #                + self.widget.value[0].getPrecisionString(rx, 2) + "/div"
-    #            dc.DrawText(xString,
-    #                        x, 0)
-    #            dc.SetPen(wx.Pen("GREY", 1))
-    #            first = False
-
     def drawPlot(self, dc, name, points):
         (winx, winy) = self.GetSize()
         if self.xMax != self.xMin:
@@ -763,7 +659,6 @@ class CorbomiteGuiWidgetTraceIn(CorbomiteGuiWidget):
         brush = wx.Brush(c, wx.SOLID)
         dc.SetBackground(brush)
         dc.Clear()
-        # self.drawScale(dc)
         self.scale.render(dc)
         clist = ['BLACK', 'BROWN', 'RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE',
                  'VIOLET', 'GREY', 'WHITE']
